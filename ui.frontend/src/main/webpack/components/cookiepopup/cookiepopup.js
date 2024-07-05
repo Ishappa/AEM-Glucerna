014d536b2b263d3b413d230679aaff1c16ae7d17
SwaroopCodilar <swaroop.k@codilar.com>
2024-07-05 12:38:10 +0530

    Cookie Popup component done

---
+++ ui.frontend/src/main/webpack/components/cookiepopup/cookiepopup.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById("myModal");
+    const closeicon = document.getElementsByClassName("modal-content-close")[0];
+    let popupClose = localStorage.getItem('popupclose') === 'true';
+    let alreadyMem = localStorage.getItem('alreadymem') === 'true';
+
+    // Only display the modal if the conditions are met
+    if (!popupClose && !alreadyMem) {
+        modal.style.display = "block";
+    }
+
+    closeicon.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    const btnDsn = document.querySelector("#btn-design");
+    const thanks = document.querySelector("#thanks");
+
+    btnDsn.addEventListener('click', function() {
+        localStorage.setItem('popupclose', 'true');
+        modal.style.display = "none";
+    });
+
+    thanks.addEventListener('click', function() {
+        localStorage.setItem('alreadymem', 'true');
+        modal.style.display = "none";
+    });
+});

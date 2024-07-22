714a80a3760c37e847a12f5dc9b95a9272659868
Ishwar <ishwarkuri4@gmail.com>
2024-07-22 16:46:39 +0530

    error fixed for getting null value

--- ui.frontend/src/main/webpack/components/backtotopButton/backToTopButton.js
+++ ui.frontend/src/main/webpack/components/backtotopButton/backToTopButton.js
@@ -1,8 +1,12 @@
 
-  document.getElementById('btn-link').addEventListener('click', function(event) {
+const backtotop = document.getElementById('btn-link');
+  
+if(backtotop){
+document.getElementById('btn-link').addEventListener('click', function(event) {
     event.preventDefault();
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   });
+}
